b83b7f1ba3b285350897bb6c0080114ae218f5fc
Mathew Kleppin <matkle414@gmail.com>
2023-10-24 14:29:56 -0400

    Update ReactDOMHostConfig.js

--- packages/react-dom/src/client/ReactDOMHostConfig.js
+++ packages/react-dom/src/client/ReactDOMHostConfig.js
@@ -24,7 +24,7 @@ import {
   getInstanceFromNode as getInstanceFromNodeDOMTree,
   isContainerMarkedAsRoot,
 } from './ReactDOMComponentTree';
-export {detachDeletedInstance} from './ReactDOMComponentTree';
+export {detachDeletedInstance, detatchFiberStateNode} from './ReactDOMComponentTree';
 import {hasRole} from './DOMAccessibilityRoles';
 import {
   createElement,
